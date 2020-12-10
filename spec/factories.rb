452c9e19e769abf5d17775dc6457009a07ed284e
Phil McCarthy <philmcc2631@gmail.com>
2020-12-10 16:40:40 -0700

    Add config for factory_bot

---
+++ spec/factories.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  # factory :model do
+  #   attribute { "yeahyeah"}
+  # end
+
+  # factory :model, class: Admin do
+  #   sequence(:email) { |n| "user_#{n}@gmail.com" }
+  #   sequence(:password) { "password" }
+  #   sequence(:name) { |n| "User Name #{n}" }
+  #   role { 0 }
+  #   active { true }
+  # end
+end
