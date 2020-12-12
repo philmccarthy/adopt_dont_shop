c8fa871076cec34172c714c5ad99a1cf5c5e8e15
Phil McCarthy <philmcc2631@gmail.com>
2020-12-12 10:17:20 -0700

    Update schema with migrations, add seeds file w/ factory_bot models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_11_144337) do
+ActiveRecord::Schema.define(version: 2020_12_11_193629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,14 +24,14 @@ ActiveRecord::Schema.define(version: 2020_12_11_144337) do
 
   create_table "applications", force: :cascade do |t|
     t.string "name"
-    t.string "street_1"
-    t.string "street_2"
+    t.string "street"
     t.string "city"
     t.string "state"
     t.string "postal_code"
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0
   end
 
   create_table "pets", force: :cascade do |t|
