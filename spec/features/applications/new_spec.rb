2bbf658f6ada49b4f55200d2227a86ccb1667305
Phil McCarthy <philmcc2631@gmail.com>
2020-12-13 11:51:19 -0700

    Add application new spec

---
+++ spec/features/applications/new_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe 'application new page', type: :feature do
+  describe 'as a visitor i see a form' do
+    it 'creates new application when submitted' do
+      visit new_application_path
+
+      fill_in 'Name', with: 'Bob Bobson'
+      fill_in 'Street', with: '1234 Real Street'
+      fill_in 'City', with: 'Bobton'
+      fill_in 'State', with: 'CO'
+      fill_in 'Postal code', with: '80210'
+      
+      before = Application.last
+      
+      click_button 'Submit'
+
+      expect(page.status_code).to eq(200)
+      expect(Application.last).to_not eq(before)
+    end
+
+    it 'cannot create an application if any field is missing' do
+      visit new_application_path
+
+      click_button 'Submit'
+
+      expect(page).to have_content("Application not created. Required information is missing.")
+    end
+  end
+end
