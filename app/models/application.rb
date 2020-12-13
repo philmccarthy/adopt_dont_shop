f234facfcf24470eb3eeb3f3d72aa14926f757b1
Phil McCarthy <philmcc2631@gmail.com>
2020-12-12 17:32:49 -0700

    Add dependent destroy for shelter and initial validates to application

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,5 +2,12 @@ class Application < ApplicationRecord
   has_many :application_pets
   has_many :pets, through: :application_pets
 
+  validates_presence_of :name,
+                        :street,
+                        :city,
+                        :state,
+                        :postal_code,
+                        :description
+
   enum status: ['In Progress', 'Pending', 'Approved', 'Rejected']
 end
