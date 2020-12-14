854dce46ff15712bd006b5057325c49d6294bf3f
Phil McCarthy <philmcc2631@gmail.com>
2020-12-14 08:14:48 -0700

    Fix expectation in admin/show feature test

--- spec/features/admin/applications/show_spec.rb
+++ spec/features/admin/applications/show_spec.rb
@@ -4,14 +4,18 @@ RSpec.describe 'admin application show page', type: :feature do
   describe 'it displays application info with approve and reject buttons for pets' do
     before(:each) do
       @app_1 = create(:application)
-      @pet = create(:pet, name: 'Bruiser')
-      @pet = create(:pet, name: 'Blanche')
-      @pet = create(:pet, name: 'Bowwow')
+      @pet_1 = create(:pet, name: 'Bruiser')
+      @pet_2 = create(:pet, name: 'Blanche')
+      @pet_3 = create(:pet, name: 'Bowwow')
     end
 
     it 'can approve and reject specific pets on an application' do
-      expect(page).to have_button('Approve')
-      expect(page).to have_button('Reject')
+      ApplicationPet.create!(application: @app_1, pet: @pet_1)
+      
+      visit admin_application_path(@app_1)
+      
+      expect(page).to have_link('APPROVE')
+      expect(page).to have_link('REJECT')
     end
   end
 end
