8c7a19a64e2e0292daf5c35479d7a2333a5beae0
Phil McCarthy <philmcc2631@gmail.com>
2020-12-14 09:13:08 -0700

    Add control flow to avoid marking 'in progress' applications as 'rejected' when visited by admin

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -2,9 +2,9 @@ class Admin::ApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
     @app_pets = ApplicationPet.find_by_app(@application.id)
-    if @application.is_rejected?
+    if @application.Pending? && @application.is_rejected?
       @application.approve
-    elsif @application.is_approved?
+    elsif @application.Pending? && @application.is_approved?
       @application.reject
     end
   end
