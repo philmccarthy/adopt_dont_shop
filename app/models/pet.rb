f09b56a7468b1c05c0d288eb92381ad6aec563a0
Phil McCarthy <philmcc2631@gmail.com>
2020-12-14 19:22:11 -0700

    Fix typo in pet.rb

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,5 +1,6 @@
 class Pet < ApplicationRecord
   belongs_to :shelter
+  has_many :application_pets, dependent: :destroy
   has_many :applications, through: :application_pets
   
   validates_presence_of :name, :description, :approximate_age, :sex
