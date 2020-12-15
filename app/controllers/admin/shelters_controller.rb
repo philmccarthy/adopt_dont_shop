aca44a48d63daff91b2092cc1b6c26bae4ff396f
Phil McCarthy <philmcc2631@gmail.com>
2020-12-15 14:30:14 -0700

    Add admin shelter show action

--- app/controllers/admin/shelters_controller.rb
+++ app/controllers/admin/shelters_controller.rb
@@ -2,4 +2,8 @@ class Admin::SheltersController < ApplicationController
   def index
     @shelters = Shelter.desc_by_name
   end
+
+  def show
+    @shelter = Shelter.name_and_address(params[:id]).first
+  end
 end
