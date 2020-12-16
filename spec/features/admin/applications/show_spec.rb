943d0d83f713ae594ceb5d98b7c9cd7848f7d576
Phil McCarthy <philmcc2631@gmail.com>
2020-12-16 08:42:43 -0700

    Update tests to cover all features developed thru browser testing

--- spec/features/admin/applications/show_spec.rb
+++ spec/features/admin/applications/show_spec.rb
@@ -9,9 +9,11 @@ RSpec.describe 'admin application show page', type: :feature do
       @pet_3 = create(:pet, name: 'Bowwow')
     end
 
-    it 'can approve and reject specific pets on an application' do
+    it 'can approve and reject specific pets on an application to reject an application' do
       app_pet = ApplicationPet.create(application: @app_1, pet: @pet_1)
       app_pet = ApplicationPet.create(application: @app_1, pet: @pet_2)
+      app_pet = ApplicationPet.create(application: @app_1, pet: @pet_3)
+
       visit admin_application_path(@app_1)
       
       within("#admin-tools-#{@pet_1.id}") do
@@ -19,6 +21,33 @@ RSpec.describe 'admin application show page', type: :feature do
         expect(page).to have_link('REJECT')
         click_link('APPROVE')
         expect(page).to have_content('APPROVED')
+      end
+
+      within("#admin-tools-#{@pet_2.id}") do
+        click_link('REJECT')
+        expect(page).to have_content('REJECTED')
+      end
+
+      within("#admin-tools-#{@pet_3.id}") do
+        click_link('REJECT')
+      end
+
+      within("#app-#{@app_1.id}") do
+        expect(page).to have_content('Rejected')
+      end
+    end
+
+    it 'can approve all app_pets on an application to approve an application' do
+      app_pet = ApplicationPet.create(application: @app_1, pet: @pet_1)
+
+      visit admin_application_path(@app_1)
+      
+      within("#admin-tools-#{@pet_1.id}") do
+        click_link('APPROVE')
+      end
+
+      within("#app-#{@app_1.id}") do
+        expect(page).to have_content('Approved')
       end
     end
   end
