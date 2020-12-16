f8a0463178dbb05296b72e74f25de5eba00b74e3
Phil McCarthy <philmcc2631@gmail.com>
2020-12-16 08:41:52 -0700

    Add shelter model tests for SQL & AR class methods

--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -29,5 +29,28 @@ describe Shelter, type: :model do
         expect(deets.zip).to eq(shelter1.zip)
       end
     end
+
+    describe '::pending_applications' do
+      it 'returns shelters with pending applications ordered by name' do
+        shelter_1 = create(:shelter, name: 'Xylophone')
+        shelter_2 = create(:shelter, name: 'Saxophone')
+        shelter_3 = create(:shelter, name: 'Alphabet')
+        app_1 = create(:application, status: 'Pending')
+        pet_1 = create(:pet, name: 'Bruiser', shelter: shelter_1)
+        pet_2 = create(:pet, name: 'Blanche', shelter: shelter_2)
+        pet_3 = create(:pet, name: 'Bowwow', shelter: shelter_3)
+        ApplicationPet.create(application: app_1, pet: pet_1)
+        ApplicationPet.create(application: app_1, pet: pet_2, status: 0)
+        ApplicationPet.create(application: app_1, pet: pet_3)
+
+        expect(Shelter.pending_applications).not_to include(shelter_2)
+        expect(Shelter.pending_applications).to include(shelter_1)
+        expect(Shelter.pending_applications).to include(shelter_3)
+        expect(Shelter.pending_applications.first).to eq(shelter_3)
+        expect(Shelter.pending_applications.last).to eq(shelter_1)
+      end
+    end
   end
 end
+
+
